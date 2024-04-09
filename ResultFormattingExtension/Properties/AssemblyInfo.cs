ad4ad7c37cbe5eefa6256ddc69308f62f8022416
one1erp <avigaile@one1.co.il>
2024-04-09 09:08:48 +0300

    Initial commit with additional files

---
+++ ResultFormattingExtension/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ResultFormattingExtension")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ResultFormattingExtension")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6BA13AB5-2D93-49AA-81BB-38D282311CB2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ResultFormattingExtension/ResultFormattingExtension.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.InteropServices;
+using LSEXT;
+using System;
+
+namespace ResultFormattingExtension
+{
+    [ComVisible(true)]
+    [ProgId("ResultFormattingExtension.ResultFormattingExtension")]
+    public class ResultFormattingExtension : IResultFormat
+    {
+        public ResultFormattingExtension(){}
+      
+        public ResultEntryFormat Format(ref LSExtensionParameters Parameters, ResultEntryPhase Phase)
+        {
+            if (Phase == ResultEntryPhase.reConvertUnits)
+            {
+                try
+                {
+                    double rawNumericResult = Parameters.Parameter("raw_numeric_result").Value;
+                    double dilutionFactor = Parameters.Parameter("dilution_factor").Value;
+                    dilutionFactor = dilutionFactor == 0 ? 1 : dilutionFactor;
+                    if (FractionalPart(dilutionFactor) == 5)
+                    {
+                        Parameters.Parameter("calculated_numeric_result").Value = rawNumericResult * 50;
+                    }
+                    else
+                    {
+                        Parameters.Parameter("calculated_numeric_result").Value = rawNumericResult*
+                                                                                  Math.Pow(10, dilutionFactor);
+                    }
+                    return LSEXT.ResultEntryFormat.rfSkipDefault;
+                }
+                catch(COMException exception)
+                {
+                //    Logger.WriteLogFile(exception);
+                    
+                }
+            }
+            return LSEXT.ResultEntryFormat.rfDoDefault;
+        }
+
+        public ResultFieldChange FieldChange(ref LSExtensionParameters Parameters)
+        {
+            return LSEXT.ResultFieldChange.rcAllow;
+        }
+        private  Int32 FractionalPart(double n)
+        {
+            string s = n.ToString("#.#########", System.Globalization.CultureInfo.InvariantCulture);
+            return Int32.Parse(s.Substring(s.IndexOf(".") + 1));
+        }
+    }
+}
+
